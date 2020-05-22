995494fe6c78128914a5a15351c2c32c1e9f8ee6
Luis Chiang <lhc4@njit.edu>
2020-05-22 18:53:17 -0400

    swapchain choose format and color space

--- platforms/baka_vulkan/include/baka_vk_swapchain.h
+++ platforms/baka_vulkan/include/baka_vk_swapchain.h
@@ -5,6 +5,9 @@
 #include <vulkan/vulkan.h>
 #include "baka_vk_utils.h"
 
+#define BAKA_VK_FORMAT_DEFAULT VK_FORMAT_B8G8R8A8_SRGB
+#define BAKA_VK_COLOR_SPACE_DEFAULT VK_COLOR_SPACE_SRGB_NONLINEAR_KHR
+
 namespace baka
 {
     class VulkanSwapchain
@@ -12,6 +15,33 @@ namespace baka
     public:
         VulkanSwapchain();
         VulkanSwapchain(VkPhysicalDevice device, VkSurfaceKHR surface);
+        ~VulkanSwapchain();
+
+        // virtual void Init();
+
+        /* STATIC METHODS */
+        static VkSurfaceFormatKHR ChooseSurfaceFormatDefault(
+            const VkSurfaceFormatKHR *surfaceFormats, 
+            uint32_t surfaceFormatCount,
+            VkFormat preferedFormat,
+            VkColorSpaceKHR preferedColorSpace);
+
+        static VkPresentModeKHR ChoosePresentMode(
+            const VkPresentModeKHR *modes, 
+            uint32_t modesCount, 
+            VkPresentModeKHR prefered);
+
+    public:
+        VkSurfaceFormatKHR (*choose_surface_format)(
+            const VkSurfaceFormatKHR *surfaceFormats, 
+            uint32_t surfaceFormatCount,
+            VkFormat preferedFormat,
+            VkColorSpaceKHR preferedColorSpace);
+
+        VkPresentModeKHR (*choose_present_mode)(
+            const VkPresentModeKHR *modes, 
+            uint32_t modesCount, 
+            VkPresentModeKHR prefered);
 
     public:
         VkPhysicalDevice gpu;
--- platforms/baka_vulkan/src/baka_vk_swapchain.cpp
+++ platforms/baka_vulkan/src/baka_vk_swapchain.cpp
@@ -1,11 +1,11 @@
+#include <cstring>
 #include "baka_vk_swapchain.h"
 
 namespace baka
 {
     VulkanSwapchain::VulkanSwapchain()
     {
-        this->support = {};
-        this->surface = VK_NULL_HANDLE;
+        std::memset(this, 0, sizeof(VulkanSwapchain));
     }
 
     VulkanSwapchain::VulkanSwapchain(VkPhysicalDevice gpu, VkSurfaceKHR surface)
@@ -13,5 +13,68 @@ namespace baka
         this->surface = surface;
         this->gpu = gpu;
         this->support = VulkanUtils::GetSwapchainSwpport(gpu, surface);
+
+        if(!this->choose_surface_format) 
+            this->choose_surface_format = VulkanSwapchain::ChooseSurfaceFormatDefault;
+        
+        if(!this->choose_present_mode)
+            this->choose_present_mode = VulkanSwapchain::ChoosePresentMode;
+    }
+
+    VulkanSwapchain::~VulkanSwapchain()
+    {
+
+    }
+
+    VkSurfaceFormatKHR VulkanSwapchain::ChooseSurfaceFormatDefault(
+        const VkSurfaceFormatKHR *surfaceFormats, 
+        uint32_t surfaceFormatCount,
+        VkFormat preferedFormat,
+        VkColorSpaceKHR preferedColorSpace)
+    {
+        VkSurfaceFormatKHR format = {};
+        int i = 0;
+        
+        if(!surfaceFormats) return {};
+
+        for(i = 0; i < surfaceFormatCount; i++)
+        {
+            format = surfaceFormats[i];
+            if( format.colorSpace == preferedColorSpace && 
+                format.format == preferedFormat )
+            {
+                return format;
+            }
+        }
+
+        return surfaceFormats[0];
+    }
+
+    /*  The present mode is what determines how the images in the swapchain are going to be presented.
+        There are a couple of modes, like:
+            VK_PRESENT_MODE_FIFO_KHR
+            VK_PRESENT_MODE_MAILBOX_KHR 
+            
+        The first one says that images should be present in an order of first in first out. It works like a queue, 
+        but the problem is that the program has to wait if the queue is full.
+        The second one is another version of the fist mode. The difference is that, once the queue is full, the 
+        old images will simply will replaced with the new ones, intead of waiting for the old ones to be rendered.
+        
+        Mailbox is the only mode that is guaranteed to be supported. 
+        
+        There are other present modes, and you can read about them from here:
+            https://vulkan-tutorial.com/Drawing_a_triangle/Presentation/Swap_chain*/
+    VkPresentModeKHR VulkanSwapchain::ChoosePresentMode(const VkPresentModeKHR *modes, uint32_t modeCount, VkPresentModeKHR preferedMode)
+    {
+        VkPresentModeKHR presentMode = {};
+
+        uint32_t i = 0;
+        for(i = 0; i < modeCount; i++)
+        {
+            presentMode = modes[i];
+            if(presentMode == preferedMode) return presentMode;
+        }
+
+        return VK_PRESENT_MODE_MAILBOX_KHR;
     }
 } // namespace baka
