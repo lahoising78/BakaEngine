5e08c5c9d3fd8aabdfaa22191c3dd452399598bf
Luis Chiang <lhc4@njit.edu>
2020-12-14 01:30:28 -0400

    icosahedron base

--- src/baka_mesh.cpp
+++ src/baka_mesh.cpp
@@ -11,6 +11,7 @@ namespace baka
 {
     static std::unordered_map<const char*,Mesh*> meshes;
     Mesh *CubePrimitive(VertexBufferLayout &layout);
+    Mesh *SpherePrimitive(VertexBufferLayout &layout);
     
     Mesh *Mesh::Create(VertexBuffer *vertexBuffer, VertexBufferLayout &layout, IndexBuffer *indexBuffer)
     {
@@ -41,6 +42,11 @@ namespace baka
         case Primitive::CUBE: 
             ret = CubePrimitive(defaultLayout);
             break;
+
+        case Primitive::SPHERE:
+            ret = SpherePrimitive(defaultLayout);
+            break;
+
         default: return nullptr;
         }
 
@@ -79,5 +85,53 @@ namespace baka
         VertexBuffer *vertexBuffer = VertexBuffer::Create(vertices, sizeof(vertices));
         IndexBuffer *indexBuffer = IndexBuffer::Create(indices, sizeof(indices));
         return Mesh::Create(vertexBuffer, layout, indexBuffer);
+    }
+
+    Mesh *SpherePrimitive(VertexBufferLayout &layout)
+    {
+        const float X = 0.525731112119133606f;
+        const float Z = 0.850650808352039932f;
+
+        float vertices[] = {
+               -X,  0.0f,     Z,
+                X,  0.0f,     Z,
+               -X,  0.0f,    -Z,
+                X,  0.0f,    -Z,
+             0.0f,     Z,     X,
+             0.0f,     Z,    -X,
+             0.0f,    -Z,     X,
+             0.0f,    -Z,    -X,
+                Z,     X,  0.0f,
+               -Z,     X,  0.0f,
+                Z,    -X,  0.0f,
+               -Z,    -X,  0.0f
+        };
+
+        std::uint32_t indices[] = {
+            0,4,1,
+            0,9,4,
+            9,5,4,
+            4,5,8,
+            4,8,1,
+            8,10,1,
+            8,3,10,
+            5,3,8,
+            5,2,3,
+            2,7,3,
+            7,10,3,
+            7,6,10,
+            7,11,6,
+            11,0,6,
+            0,1,6,
+            6,1,10,
+            9,0,11,
+            9,11,2,
+            9,2,5,
+            7,2,11
+        };
+
+        VertexBuffer *vb = VertexBuffer::Create(vertices, sizeof(vertices));
+        IndexBuffer *ib = IndexBuffer::Create(indices, sizeof(indices));
+        return Mesh::Create(vb, layout, ib);
     }
 } // namespace baka
